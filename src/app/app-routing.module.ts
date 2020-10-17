10d436a3cb3722391748b370fc094622810d497f
jcmq6b <60238224+jcmq6b@users.noreply.github.com>
2020-10-17 01:41:34 -0500

    Adding homepage and login components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//Adding imports for componenets
+import { LoginComponent } from './components/login/login.component'
+import { HomepageComponent } from './components/homepage/homepage.component'
+
 const routes: Routes = [];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
