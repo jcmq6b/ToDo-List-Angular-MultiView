a2da069c0e36fbae06b3dc6eb296e66d27cfe024
jcmq6b <60238224+jcmq6b@users.noreply.github.com>
2020-10-17 23:55:23 -0500

    adding homepage methods, about to delete todoSubmit()

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  todoForm = this.fb.group({
+    todo: ["", Validators.required],
+  });
+
+  todoArray = [];
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  addTodo(value) {
+    if (value != '') {
+      this.todoArray.push(value);
+      console.log(this.todoArray);
+      this.todoForm.resetForm();
+    } else {
+      alert('Todo Field Required!!');
+    }
+  }
+    deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+        console.log('delete item');
+      }
+    }
+  }
+    todoSubmit(form: any) {
+      if (form != '') {
+        console.log(form);
+        this.todoArray.push(form.todo);
+        this.todoForm.resetForm();
+      } else {
+        alert('Todo Field Reqired!!');
+      }
+    }
+  
 }
